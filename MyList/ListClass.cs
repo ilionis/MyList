93c55731bcc414275ab7547713621372da82b09a
ilionis <ilionis171717@mail.ru>
2017-03-14 17:35:29 +0300

    Add files via upload

---
+++ MyList/ListClass.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyList
+{
+    public class ListClass<T>
+    {
+        public Element<T> firstElement, endElement;
+
+        //Создаём пустой список
+        public ListClass()
+        {
+            firstElement = null;
+            endElement = null;
+        }
+
+        //Создаём список с одним элементом
+        public ListClass(T t)
+        {
+            firstElement = new Element<T>(t);
+            endElement = firstElement;
+        }
+
+        //Добавляем новый элемент
+        public void Add(T t)
+        {
+            var newElement = new Element<T>(t);
+
+            if(endElement != null)
+            {
+                endElement.next = newElement;
+                newElement.last = endElement;
+                endElement = newElement;
+            }
+            else
+            {
+                firstElement = newElement;
+                endElement = newElement;
+            }
+        }
+    }
+
+    public class Element<T>
+    {
+        public Element<T> last, next;
+
+        private T varible;
+
+        public Element(T t)
+        {
+            varible = t;
+        }
+
+        //Возвращает значение элемента
+        public T Value()
+        {
+            return varible;
+        }
+
+    }
+
+}
---
+++ MyList/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           ListClass<int> MyList =  new ListClass<int>(1);
+           
+           MyList.Add(3);
+           MyList.Add(4);
+
+
+           Console.WriteLine(MyList.firstElement.Value().ToString() + " " + MyList.firstElement.next.Value().ToString() + " " + MyList.endElement.last.Value().ToString()+" " + MyList.endElement.Value().ToString());
+            // 1 3 3 4
+           Console.ReadKey();
+        }
+    }
+}
